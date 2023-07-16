0627dd408c804e647089ddede83eeee5290fd7cd
almudenariojo <103251095+almudenariojo@users.noreply.github.com>
2023-07-16 23:25:53 +0200

    mejoras

--- js/main.js
+++ js/main.js
@@ -58,4 +58,63 @@ $(document).ready(function(){
  $("#posts").append(post);
  });
 
+ //Selectorr de tema
+
+ var theme= $("#theme");
+
+ $("#to-green").click(function(){
+theme.attr("href", "css/green.css")
+ });
+
+ $("#to-red").click(function(){
+  theme.attr("href", "css/red.css")
+   });
+
+   $("#to-blue").click(function(){
+    theme.attr("href", "css/blue.css")
+     });
+
+
+// Scroll arriba de la web
+$(".subir").click(function(e){
+  e.preventDefault();
+  $("html,body").animate({
+    scrollTop:'0'
+     }, 500);
+
+     return false;
+   });
+
+// Login falso
+$("#login form").submit(function() {
+  var form_name = $("#form_name").val();
+  localStorage.setItem("form_name", form_name);
+});
+
+var form_name = localStorage.getItem("form_name");
+
+if (form_name != null && form_name != "undefined") {
+  var about_parrafo = $("#about p");
+  about_parrafo.html("<br><strong> Bienvenido " + form_name + "</strong>");
+ about_parrafo.append("<a href='#' id='logout'> Cerrar sesión </a>");
+ $("#logout").css({
+  "color": "white",
+  "background": "green",
+  "font-size": "16px",
+  "margin-left": "20px",
+  "text-decoration": "none",
+  "border": "1px solid black"
+ 
+
+});
+ $("#login").hide();
+
+ $("#logout").click(function(){
+  localStorage.clear();
+ location.reload();
+
+ })
+}
+
+
 });
